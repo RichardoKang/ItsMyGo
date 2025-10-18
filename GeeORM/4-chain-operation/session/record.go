e86e4ad3b00e566ad2fe92f4d2df3bc090035a02
Richard Kang <A.weiP@outlook.com>
2025-10-18 21:15:18 +0800

    feat: Update

---
+++ GeeORM/4-chain-operation/session/record.go
@@ -0,0 +1,125 @@
+package session
+
+import (
+	"errors"
+	"geeorm/clause"
+	"reflect"
+)
+
+func (s *Session) Insert(values ...any) (int64, error) {
+	recordValues := make([]any, 0)
+
+	for _, value := range values {
+		table := s.Model(value).RefTable()
+		s.clause.Set(clause.INSERT, table.Name, table.FieldNames) // 设置 INSERT 子句
+		recordValues = append(recordValues, table.RecordValues(value))
+	}
+
+	s.clause.Set(clause.VALUES, recordValues...) // 设置 VALUES 子句
+	sql, vars := s.clause.Build(clause.INSERT, clause.VALUES)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected() // 返回插入的记录数, nil
+}
+
+func (s *Session) Find(values any) error {
+	destSlice := reflect.Indirect(reflect.ValueOf(values))                // 获取切片的反射值
+	destType := destSlice.Type().Elem()                                   // 获取切片元素的类型
+	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable() // 获取切片元素类型的表信息
+
+	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)                              // 设置 SELECT 子句
+	sql, vars := s.clause.Build(clause.SELECT, clause.WHERE, clause.ORDERBY, clause.LIMIT) // 构建 SQL 语句
+	rows, err := s.Raw(sql, vars...).QueryRows()                                           // 执行查询
+	if err != nil {
+		return err
+	}
+
+	// 遍历结果集
+	for rows.Next() {
+		dest := reflect.New(destType).Elem() // 创建切片元素类型的实例
+		var values []any
+		for _, name := range table.FieldNames {
+			values = append(values, dest.FieldByName(name).Addr().Interface()) // 获取字段地址
+		}
+		if err := rows.Scan(values...); err != nil { // 扫描行数据到字段
+			return err
+		}
+		destSlice.Set(reflect.Append(destSlice, dest)) // 将实例添加到切片中
+	}
+	return rows.Close()
+}
+
+func (s *Session) Update(kv ...any) (int64, error) {
+	m, ok := kv[0].(map[string]any) // 把kv[0]断言为map[string]any类型,eg:源数据为 "Name", "Tom", "Age", 18
+
+	if !ok {
+		m = make(map[string]any)
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
+	}
+
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func (s *Session) Delete() (int64, error) {
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	row := s.Raw(sql, vars...).QueryRow()
+	var tmp int64
+	if err := row.Scan(&tmp); err != nil {
+		return 0, err
+	}
+	return tmp, nil
+}
+
+func (s *Session) Limit(num int) *Session {
+	s.clause.Set(clause.LIMIT, num)
+	return s
+}
+
+func (s *Session) Where(desc string, args ...any) *Session {
+	var vars []any
+	s.clause.Set(clause.WHERE, append(append(vars, desc), args...)...)
+	return s
+}
+
+func (s *Session) OrderBy(desc string) *Session {
+	s.clause.Set(clause.ORDERBY, desc)
+	return s
+}
+
+func (s *Session) First(value any) error {
+	dest := reflect.Indirect(reflect.ValueOf(value))
+	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
+
+	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
+		return err
+	}
+
+	if destSlice.Len() == 0 {
+		return errors.New("Not Found")
+	}
+
+	dest.Set(destSlice.Index(0))
+	return nil
+}
